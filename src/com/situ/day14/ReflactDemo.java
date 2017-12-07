0adb1e910eb128eceadda2b2ca0d842d1aff257d
GaoChengQuan <529390053@qq.com>
2017-12-07 10:52:20 +0800

    day14--反射2

--- src/com/situ/day14/ReflactDemo.java
+++ src/com/situ/day14/ReflactDemo.java
@@ -1,6 +1,7 @@
 package com.situ.day14;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 import org.junit.Test;
@@ -25,10 +26,35 @@ public class ReflactDemo {
 		System.out.println(person);
 	}
 	
-	
-	
-	
-	
-	
+	@Test
+	public void test2() throws Exception {
+		Teacher teacher = new Teacher();
+		System.out.println(teacher);
+		changeField(teacher);
+		System.out.println(teacher);
+	}
+	//写一个函数，传入任意的对象，
+	//可以将对象里面String类型的属性中的a改成b。
+	private void changeField(Object object) throws Exception {
+		//1.获取对应的Class
+		Class clazz = object.getClass();
+		//2.获得所有的Fields[]
+		Field[] fields = clazz.getDeclaredFields();
+		//3.遍历所有的Fields[],如果是String类型的，将包含b的改为a
+		for (Field field : fields) {
+			//3.1 判断如果是String.class才修改
+			if (field.getType() == String.class) {
+				//3.2 获得Field的值
+				field.setAccessible(true);
+				//person.getName()/person.name
+				String oldValue = (String) field.get(object);
+				//3.3 将获得的值中的a改为b
+				String newValue = oldValue.replace("a", "b");
+				//3.4将修改后的值设置到Field
+				//person.setName("zhansgan")
+				field.set(object, newValue);
+			}
+		}
+	}
 	
 }
---
+++ src/com/situ/day14/Teacher.java
@@ -0,0 +1,12 @@
+package com.situ.day14;
+
+public class Teacher {
+	private String name = "android";
+	private Integer age = 19;
+	private String address = "qingdao";
+	
+	@Override
+	public String toString() {
+		return "Teacher [name=" + name + ", age=" + age + ", address=" + address + "]";
+	}
+}
